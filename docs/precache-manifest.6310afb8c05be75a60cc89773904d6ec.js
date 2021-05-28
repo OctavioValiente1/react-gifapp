12736959b1498f6179aac28010ba8b9eceac3c9a
Octavio Valiente <octaavaliente@gmail.com>
2021-05-27 21:16:29 -0300

    docs agregado

---
+++ docs/precache-manifest.6310afb8c05be75a60cc89773904d6ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b0ab87324656dac94d7224d159998e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e098a0ee4d2cbb27df84",
+    "url": "/static/css/main.b30f6c73.chunk.css"
+  },
+  {
+    "revision": "9162cdb7b1964b5020c7",
+    "url": "/static/js/2.fd2377de.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.fd2377de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e098a0ee4d2cbb27df84",
+    "url": "/static/js/main.68be19d7.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
